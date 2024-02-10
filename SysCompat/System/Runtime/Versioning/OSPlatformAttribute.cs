77c2a326b257f17c0f8fb30dc6859b2b4b38eadb
Jason Curl <jcurl@arcor.de>
2024-02-10 19:39:15 +0100

    Add RJCP.Core.SysCompat for .NET Framework
    The RJCP.Core.SysCompat contains a single attribute, `SupportedOsPlatform`, that is compatible with the same attribute used in .NET 5 and later. This is to allow extending code (with no functionality) for compatibility with projects multitargetting .NET Framework and .NET Core.
    
    Issue: DOTNET-938

---
+++ SysCompat/System/Runtime/Versioning/OSPlatformAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace System.Runtime.Versioning
+{
+    using System;
+
+    /// <summary>
+    /// Base type for all platform-specific API attributes.
+    /// </summary>
+    public abstract class OSPlatformAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OSPlatformAttribute"/> class.
+        /// </summary>
+        /// <param name="platformName">Name of the platform.</param>
+        internal protected OSPlatformAttribute(string platformName)
+        {
+            PlatformName = platformName;
+        }
+
+        /// <summary>
+        /// Gets the name and optional version of the platform that the attribute applies to.
+        /// </summary>
+        /// <value>The name and optional version of the platform that the attribute applies to.</value>
+        public string PlatformName { get; }
+    }
+}
---
+++ SysCompat/System/Runtime/Versioning/SupportedOSPlatformAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace System.Runtime.Versioning
+{
+    using System;
+
+    /// <summary>
+    /// Indicates that an API is supported for a specified platform or operating system. If a version is specified, the
+    /// API cannot be called from an earlier version. Multiple attributes can be applied to indicate support on multiple
+    /// operating systems.
+    /// </summary>
+    /// <remarks>
+    /// Callers can apply a SupportedOSPlatformAttribute or use guards to prevent calls to APIs on unsupported operating
+    /// systems. A given platform should only be specified once.
+    /// <para>This attribute is provided for compatibility to older frameworks. It isn't used anywhere.</para>
+    /// </remarks>
+    [AttributeUsage(
+        AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor |
+        AttributeTargets.Enum | AttributeTargets.Event | AttributeTargets.Field |
+        AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Module |
+        AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
+    public sealed class SupportedOSPlatformAttribute : OSPlatformAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportedOSPlatformAttribute"/> class for the specified
+        /// supported OS platform..
+        /// </summary>
+        /// <param name="platformName">The supported OS platform name, optionally including a version.</param>
+        public SupportedOSPlatformAttribute(string platformName)
+            : base(platformName) { }
+    }
+}
