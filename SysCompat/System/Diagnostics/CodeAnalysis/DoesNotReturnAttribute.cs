c1a93288ae0d69f322cd8f40f2777d7f2ce09633
Jason Curl <jcurl@arcor.de>
2024-02-15 22:53:52 +0100

    SysCompat: Provide attribute [DoesNotReturn]
    Even if this attribute is provided, the .NET Roslyn compiler won't use it unless you opt in to nullable.
    
    Issue: DOTNET-961

---
+++ SysCompat/System/Diagnostics/CodeAnalysis/DoesNotReturnAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// Specifies that a method will never return under any circumstance.
+    /// </summary>
+    /// <remarks>
+    /// For more information, see Nullable static analysis in the C# guide.
+    /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/attributes/nullable-analysis
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class DoesNotReturnAttribute : Attribute { }
+}
